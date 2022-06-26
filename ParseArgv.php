6befd3e3c318272e5ab1d7b4c1a486c82a46075d
Dennis Iversen <dennis.iversen@gmail.com>
2022-06-26 12:37:53 +0200

    refactor

--- ParseArgv.php
+++ ParseArgv.php
@@ -3,81 +3,147 @@
 namespace Diversen;
 
 /**
- * Parse argv and get '-' and '--' flags and values, and get any value without a flag.
+ * Parse argv and return an array with the options and arguments.
  */
 class ParseArgv {
     
-        
-    
     /**
-     * var holding $flags as key => value 
+     * var holding $options as key => value 
      * @var array 
      */
-    public $flags = array ();
+    public $options = array ();
     
     /**
-     * var holding any values without flags
+     * var holding any arguments without options
      * @var array 
      */
-    public $values = array ();
+    public $arguments = array ();
     
     /**
      * Construct and parse global argv
      */
-    public function __construct() {
-        $this->parse();
+    public function __construct(array $argv_ = null) {
+        $this->parse($argv_);
     }
     
     /**
-     * Parse argv
+     * Parse argv. If $argv_ is not set then use global $argv
      * @global array $argv
      */
-    public function parse() {
-        global $argv;
-
-        $args = $argv;
+    public function parse(array $argv_ = null) {
+        
+        if (!$argv_) {
+            global $argv;
+            $argv_ = $argv;
+        }
         
         // Don't care about the php file
-        unset($args[0]);
+        unset($argv_[0]);
 
-        $i = 0;
-        
-        foreach ($args as $arg) {
-            // - and -- are commands
+        foreach ($argv_ as $arg) {
+
+            // Get commands ('-', '--')
             if (preg_match("/^[-]{1,2}/", $arg)) {
                 $arg = preg_replace("/^[-]{1,2}/", '', $arg);
-                
-                $flag = $this->getFlagKey($arg);
-                $value = $this->getFlagValue($arg);
-                $this->flags[$flag] = $value;
-            } else {
-                $this->values[$i] = $arg;
-                $i++;
+                $option = $this->getOptionKey($arg);
+                $value = $this->getOptionsValue($arg);
+                $this->options[$option] = $value;
+            } 
+            
+            // Get arguments
+            else {
+                $this->arguments[] = $arg;
             }
         }
     }
     
     /**
-     * Get flag key from arg
+     * Get option key from option string
      * @param string $arg
      * @return string $value
      */
-    private function getFlagKey ($arg) {
+    private function getOptionKey ($arg) {
         $ary = explode('=', $arg);
         return $ary[0];
     }
     
     /**
-     * Get flag value from arg
+     * Get option value from option string
      * @param string $arg
      * @return string
      */
-    private function getFlagValue ($arg) {
+    private function getOptionsValue ($arg) {
         $ary = explode('=', $arg);
         if (empty($ary[1])) {
             return '';
         }
         return $ary[1];
     }
+    
+    
+    /**
+     * Return a option from options. If the option is not set return 'null'
+     * If the option is set return the option value as a string. If the option is set
+     * but does not have any value return true
+     */
+    public function getOption ($option) {
+        if (isset($this->options[$option])) {
 
+            // Flag exists, but no value
+            if ($this->options[$option] === '') {
+                return true;
+            }
+
+            // Flag has a value
+            return $this->options[$option];
+        }
+    }
+
+    /**
+     * Checks if at least one option in a given array of options is set in the argv options
+     */
+    public function inOptions($options) {
+        $argv_options = array_keys($this->options);
+        foreach($argv_options as $option) {
+            if (in_array($option, $options)) {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+
+    /**
+     * Does an option exist, by value, e.g. 'help'
+     */
+    public function optionExists ($option) {
+        return isset($this->options[$option]);
+    }
+    
+    /**
+     * Does an argument exists (by index, e.g. 0)
+     */
+    public function argumentExists ($key) {
+        if (isset($this->arguments[$key])) {
+            return true;
+        }
+    }
+    
+    /**
+     * Get argument by index (e.g. 0)
+     */
+    public function getArgument ($key) {
+        if (isset($this->arguments[$key])) {
+            return $this->arguments[$key];
+        }
+    }
+    
+    /**
+     * Unset a value from arguments_by_key. 
+     */
+    public function unsetArgument($key) {
+        unset($this->arguments[$key]);
+
+        $this->arguments = array_values($this->arguments);
+    }
 }
--- test.php
+++ test.php
@@ -2,7 +2,68 @@
 
 include_once "ParseArgv.php";
 use Diversen\ParseArgv;
-$p = new ParseArgv();
-print_r($p->flags);
-print_r($p->values);
 
+function message($message) {
+    echo $message . "\n";
+}
+
+// You can specify argv in the constructor:
+$args = array (
+    0 => 'test.php',
+    1 => '-h',
+    2 => '--help',
+    3 => '--message=hello',
+    4 => 'argument1',
+    5 => 'argument2',
+);
+
+// If not specified then use global $argv
+// The following gives the same argv as the above:
+// php test.php  -h --help --message=hello argument1 argument2
+
+// Use global argv
+$p = new ParseArgv();
+
+// Unset the first argument (the php file name or the CLI program name)
+// $p->unsetArgument(0);
+
+// Options are 
+message('Options are:');
+var_dump($p->options);
+// ->
+// array(3) {
+//     ["h"]=>
+//     string(0) ""
+//     ["help"]=>
+//     string(0) ""
+//     ["message"]=>
+//     string(5) "hello"
+//   }
+  
+message('arguments are:');
+var_dump($p->arguments);
+// ->
+// array(2) {
+//     [0]=>
+//     string(9) "argument1"
+//     [1]=>
+//     string(9) "argument2"
+//   }
+
+message("Value of option 'h'");
+var_dump($p->getOption('h'));
+// -> true
+
+message("Value of option 'message'");
+var_dump($p->getOption('message'));
+// -> "hello"
+
+message("Does option 'h' exist");
+var_dump($p->optionExists('h'));
+// -> true
+
+message('Value of argument 1');
+var_dump($p->getArgument(0));
+// -> argument1
+
+var_dump($p->argumentExists(0));
