770b235575c747cd3ef84286bbd936b7d5277203
Dennis Iversen <dennis.iversen@gmail.com>
2023-03-02 17:55:48 +0100

    format code. castOptions as public method

--- ParseArgv.php
+++ ParseArgv.php
@@ -4,29 +4,32 @@ declare(strict_types=1);
 
 namespace Diversen;
 
-class ParseArgv {
-    
+class ParseArgv
+{
 
-    public array $options = array ();
-    
-    public array $arguments = array ();
+
+    public array $options = array();
+
+    public array $arguments = array();
 
     public string $command_name = '';
-    
-    public function __construct(array $argv = null, array $cast = array ()) {
+
+    public function __construct(array $argv = null, array $cast = array())
+    {
         $this->parse($argv, $cast);
     }
-    
+
     /**
      * Parse argv. If $argv is not set then use global $argv
      * @global array $argv
      */
-    private function parse(array $argv = null, array $cast = array ()) {
-        
+    private function parse(array $argv = null, array $cast = array())
+    {
+
         if (!$argv) {
             global $argv;
         }
-        
+
         foreach ($argv as $arg) {
 
             // Get commands ('-', '--')
@@ -35,8 +38,8 @@ class ParseArgv {
                 $option = $this->getOptionKey($arg);
                 $value = $this->getOptionsValue($arg);
                 $this->options[$option] = $value;
-            } 
-            
+            }
+
             // Get arguments
             else {
                 $this->arguments[] = $arg;
@@ -51,18 +54,20 @@ class ParseArgv {
     /*
      * Cast options to a specific type
     */
-    private function castOptions(array $cast) {
+    public function castOptions(array $cast)
+    {
         foreach ($cast as $key => $type) {
             if (isset($this->options[$key])) {
                 $this->options[$key] = $this->castValue($this->options[$key], $type);
             }
-        }    
+        }
     }
 
     /*
      * Cast a value to a specific type
     */
-    private function castValue($value, $type) {
+    private function castValue($value, $type)
+    {
         switch ($type) {
             case 'int':
                 return (int) $value;
@@ -94,26 +99,29 @@ class ParseArgv {
     /**
      * Get option key from argv arg
      */
-    private function getOptionKey (string $arg): string {
+    private function getOptionKey(string $arg): string
+    {
         $ary = explode('=', $arg);
         return trim($ary[0]);
     }
-    
+
     /**
      * Get option value from argv arg
      */
-    private function getOptionsValue (string $arg): bool|string {
+    private function getOptionsValue(string $arg): bool|string
+    {
         $ary = explode('=', $arg);
         if (empty($ary[1])) {
             return true;
         }
         return trim($ary[1]);
     }
-    
+
     /**
      * Get an option
      */
-    public function getOption (string $option): mixed {
+    public function getOption(string $option): mixed
+    {
         if (isset($this->options[$option])) {
             return $this->options[$option];
         }
@@ -123,36 +131,40 @@ class ParseArgv {
     /**
      * Does an option exist, by value, e.g. 'help'
      */
-    public function optionExists (string $option): bool {
+    public function optionExists(string $option): bool
+    {
         if (isset($this->options[$option])) {
             return true;
         }
         return false;
     }
-    
+
     /**
      * Does an argument exists (by index, e.g. 0)
      */
-    public function argumentExists (int $key) {
+    public function argumentExists(int $key)
+    {
         if (isset($this->arguments[$key])) {
             return true;
         }
         return false;
     }
-    
+
     /**
      * Get argument by index (e.g. 0)
      */
-    public function getArgument (int $key) {
+    public function getArgument(int $key)
+    {
         if (isset($this->arguments[$key])) {
             return $this->arguments[$key];
         }
     }
-    
+
     /**
      * Unset a value from arguments_by_key. 
      */
-    public function unsetArgument(int $key) {
+    public function unsetArgument(int $key)
+    {
         if (isset($this->arguments[$key])) {
             unset($this->arguments[$key]);
             $this->arguments = array_values($this->arguments);
